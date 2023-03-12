1b0513a276ad35d1048575bafdf24ba4bb2dac55
Anna <shiskanovaag@mail.ru>
2023-03-12 15:10:58 +1000

    Add task 60

--- homework/Program.cs
+++ homework/Program.cs
@@ -233,4 +233,81 @@ static void Task58()
     ShowDuoArray(MultiplyArray(firstArray, secondArray));
     
 }
-Task58();
+
+static void Task60()
+{
+    /*Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+    Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+    Массив размером 2 x 2 x 2
+    66(0,0,0) 25(0,1,0)
+    34(1,0,0) 41(1,1,0)
+    27(0,0,1) 90(0,1,1)
+    26(1,0,1) 55(1,1,1)*/
+   // Random random = new Random();
+    int[,,] array =  new int[2, 2, 2];
+
+    void FillArray(int[,,] array)
+    {
+        Random random = new Random();
+        
+        
+        for ( int i = 0; i < array.GetLength(0); i++)
+        {
+            Console.WriteLine();
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                for (int k = 0; k < array.GetLength(2); k++)
+                {   
+                    array[i, j, k] = random.Next(10,100);
+                    Console.Write($"{array[i, j, k] + " "} ({i} {j} {k}) ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+        
+    }
+    FillArray(array);
+     
+
+     
+   /* void UniqueElements(int[,,] array)
+    { 
+        int tmp = array[0, 0, 0];
+        int l = 0; //count
+        Random random = new Random();
+
+        for ( int i = 0; i < array.GetLength(0); i++)
+        {
+            Console.WriteLine();
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                for (int k = 0; k < array.GetLength(2); k++)
+                {   
+                    foreach (int el in array)
+                    {
+                       if( array[i, j, k] == array[i, j, k])
+                       {
+                        array[i, j, k] = random.Next(10,100);
+                        l++;
+                       }
+                       else
+                    Console.Write($"{array[i, j, k] + " "} ({i} {j} {k}) ");
+
+                    }
+                    
+                    
+
+                    
+                }
+                Console.WriteLine();
+            }
+
+        }
+      
+        
+        
+    }
+    UniqueElements(array);*/
+}
+Task60();
